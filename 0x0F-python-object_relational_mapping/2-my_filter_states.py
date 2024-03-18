e0d0027a6af1330f8cc534e2fd621c35cdd001c2
Martinonso <martinellyck@gmail.com>
2024-03-18 05:29:17 +0100

    put

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -3,11 +3,10 @@
 in the states table of hbtn_0e_0_usa
 where name matches the argument"""
 
-if __name__ == '__main__':
-
     import MySQLdb
     import sys
 
+if __name__ == '__main__':
     db = MySQLdb.connect(host='localhost', port=3306,
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
